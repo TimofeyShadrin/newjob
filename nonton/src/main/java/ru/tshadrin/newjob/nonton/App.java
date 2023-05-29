7ee07275ba0d0924b797a381a902d2ce4b70e718
Timofei Shadrin <tshadrin@yandex.ru>
2023-05-29 15:08:15 +0300

    Git init

---
+++ nonton/src/main/java/ru/tshadrin/newjob/nonton/App.java
@@ -0,0 +1,13 @@
+package ru.tshadrin.newjob.nonton;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
